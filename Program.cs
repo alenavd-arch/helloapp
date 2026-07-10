aa1edddea7d8bd5981e4fce57ee445111c98f267
black <black@example.com>
2026-07-10 18:37:33 +0500

    Add black greeting

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
-<<<<<<< HEAD
 ﻿Console.WriteLine("hello");
 
 void GreetWhite()
 {
     Console.WriteLine("hello white");
 }
-=======
-﻿Console.WriteLine("hello everyone");
->>>>>>> eec6337 (Change greeting)
+
+void GreetBlack()
+{
+    Console.WriteLine("hello black");
+}
+
