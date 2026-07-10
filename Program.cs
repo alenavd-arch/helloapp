ca42002873cd4a511c673f665f7cf1b0abcbd60e
white <white@example.com>
2026-07-10 18:14:58 +0500

    Add white greeting

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hello");
+﻿Console.WriteLine("hello");
+
+void GreetWhite()
+{
+    Console.WriteLine("hello white");
+}
