b3d432ed1243ec3a9acab7bcb47158bcbcdaaa60
black <black@example.com>
2026-07-10 18:21:16 +0500

    Change greeting

--- Program.cs
+++ Program.cs
@@ -1,6 +1,10 @@
+<<<<<<< HEAD
 ﻿Console.WriteLine("hello");
 
 void GreetWhite()
 {
     Console.WriteLine("hello white");
 }
+=======
+﻿Console.WriteLine("hello everyone");
+>>>>>>> eec6337 (Change greeting)
